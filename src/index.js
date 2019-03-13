39f41248216a47a89af5f084175041a6a96bdb5c
Ian Cook Westgate and Maly Phongsavanh <maly@>
2019-03-13 13:17:16 -0700

    working toDateString code.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 import $ from 'jquery';
 
-const regYear = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-const leapYear = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const monthOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// const monthOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 
-class processDate {
-  constructor (month, day, year) {
-    this.month = month,
-    this.day = day,
-    this.year = year
-  }
-
-  thisMonth() {
-    let calculatedMonth = this.month - 1;
-    let foundMonth = monthOfYear[calculatedMonth];
-    return foundMonth;
-  }
-
-  weekday() {
-
-  }
-}
+// class processDate {
+//   constructor (month, day, year) {
+//     this.month = month,
+//     this.day = day,
+//     this.year = year
+//   }
+//
+//   thisMonth() {
+//     let calculatedMonth = this.month - 1;
+//     let foundMonth = monthOfYear[calculatedMonth];
+//     return foundMonth;
+//   }
+// }
 
 
 
@@ -32,10 +24,13 @@ $(document).ready(function() {
   $("button").click(function() {
     let userInput = ($("#input").val().split("-")).map(Number);
 
-    const userStuff = new processDate(userInput[0], userInput[1], userInput[2]);
+    // calculating month only
+    // const userStuff = new processDate(userInput[0], userInput[1], userInput[2]);
+    // let monthNumber = userStuff.thisMonth();
 
-    let monthNumber = userStuff.thisMonth();
+  //  Date function
 
-    console.log(monthNumber);
+    const inputStuff = new Date(userInput);
+    $(".output").text(inputStuff.toDateString());
   })
 });
